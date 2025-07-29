3699e37b5f13588a30aef336af918db7a7bf4ec5
Tommych123 <pmiltsev@mail.ru>
2025-07-29 09:17:31 +0300

    CORS allow

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,6 @@ import (
 	"fmt"
 	_ "github.com/Tommych123/auth-service/cmd/docs"
 	"github.com/Tommych123/auth-service/internal/auth"
-	_ "github.com/Tommych123/auth-service/internal/auth"
 	"github.com/Tommych123/auth-service/internal/config"
 	"github.com/Tommych123/auth-service/internal/db"
 	"github.com/swaggo/http-swagger"
@@ -19,23 +18,32 @@ import (
 	"net/http"
 )
 
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	cfg := config.LoadEnv()
-
 	database := db.NewPostgresDB(cfg)
-
 	repo := auth.NewRepository(database)
 	service := auth.NewService(repo, cfg.JWTSecret, cfg.WebhookURL)
 	handler := auth.NewHandler(service)
-
-	http.HandleFunc("/token", handler.Token)     // POST /token?user_id=...
-	http.HandleFunc("/refresh", handler.Refresh) // POST { user_id, refresh_token }
-	http.HandleFunc("/me", handler.Me)           // GET with token
-	http.HandleFunc("/logout", handler.Logout)   // POST with token
-
-	http.Handle("/swagger/", httpSwagger.WrapHandler)
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/token", handler.Token)
+	mux.HandleFunc("/refresh", handler.Refresh)
+	mux.HandleFunc("/me", handler.Me)
+	mux.HandleFunc("/logout", handler.Logout)
+	mux.Handle("/swagger/", httpSwagger.WrapHandler)
 	addr := fmt.Sprintf(":%s", cfg.Port)
 	log.Println("Server started at http://localhost" + addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, enableCors(mux)))
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.24.4
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.3
+	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
+	github.com/swaggo/http-swagger v1.3.4
+	github.com/swaggo/swag v1.16.5
 	golang.org/x/crypto v0.40.0
 )
 
@@ -14,17 +17,12 @@ require (
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
 	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
-	github.com/swaggo/http-swagger v1.3.4 // indirect
-	github.com/swaggo/swag v1.16.5 // indirect
 	golang.org/x/mod v0.26.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/tools v0.35.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
